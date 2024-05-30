80578f4e08a3e9ec1cf8bb474e637985806e1109
ankurjbs <ankurjbs@gmail.com>
2024-05-30 12:36:18 +0530

    Handling no project and new project

--- src/App.jsx
+++ src/App.jsx
@@ -19,17 +19,19 @@ function App() {
 
   function handleAddProject(projectData) {
     setProjectsState((prevState) => {
+      const projectId = Math.random();
       const newProject = {
         ...projectData,
-        id: Math.random(),
+        id: projectId,
       };
       return {
         ...prevState,
+        selectedProjectId: undefined,
         projects: [...prevState.projects, newProject],
       };
     });
   }
-  console.log(projectsState);
+
   let content;
   if (projectsState.selectedProjectId === null) {
     content = <NewProject onAdd={handleAddProject} />;
@@ -39,7 +41,10 @@ function App() {
 
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectSidebar onStartAddProject={handleStartAddProject} />
+      <ProjectSidebar
+        onStartAddProject={handleStartAddProject}
+        projects={projectsState.projects}
+      />
       {content}
     </main>
   );
